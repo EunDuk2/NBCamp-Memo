bc7ba0ca113a0b7084811331f81011d0ceb53a36
Eunsung <dmsqls6789@naver.com>
2025-02-26 16:07:05 +0900

    Step 1: 기본 UI 만들기

---
+++ Memo/Memo/MemoTableCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemoTableCell.swift
+//  Memo
+//
+//  Created by Eunsung on 2/26/25.
+//
+
+import UIKit
+
+class MemoTableCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
--- Memo/Memo/ViewController.swift
+++ Memo/Memo/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
     }
 
-
+    @IBAction func didTapPlusBarButton(_ sender: Any) {
+        
+    }
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoTableCell") as! MemoTableCell
+        
+        cell.label.text = "Hello, World!"
+        
+        return cell
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
